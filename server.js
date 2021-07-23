bfd35c80945f4f6a62d1c13dd5e22781875e58d2
Casey R <crowlandsdev@gmail.com>
2021-07-23 10:04:27 -0400

    changed route order

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const PORT = process.env.PORT || 3001
 app.use(express.JSON());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
-app.use('/', htmlRoutes);
 app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}.`);
